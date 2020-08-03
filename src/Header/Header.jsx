799b2ce028bfe9915397af7f44f22f190a768b70
Squiddymabob <squiddymabob@gmail.com>
2020-08-03 23:25:40 +0100

    navbar luinks

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="/">
         <img
           src="https://www-league.nhlstatic.com/images/logos/league-dark/133-flat.svg"
@@ -12,6 +12,25 @@ function Header() {
           alt="NHL Logo"
         />
       </a>
+      <a className="navbar-brand" href="/">NHL</a>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Link</a>
+          </li>
+        </ul>
+        <span className="navbar-text">
+          Right aligned
+        </span>
+      </div>
     </nav>
   );
 }
