0fa5864282f8b2817fa3cb426a67f8be9efb9e95
Squiddymabob <squiddymabob@gmail.com>
2020-08-03 23:01:53 +0100

    team cards

--- src/Teams/Teams.jsx
+++ src/Teams/Teams.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Teams.css';
 import { getCurrentTeams } from '../api/api';
 
 class Teams extends React.Component {
@@ -6,26 +7,45 @@ class Teams extends React.Component {
     super(props);
     this.state = {
       isLoaded: false,
+      teams: [],
     };
   }
 
   componentDidMount() {
     getCurrentTeams().then((response) => {
-      const teams = response || [];
+      const teams = response.teams || [];
       this.setState({ isLoaded: true });
-      console.log(teams);
+      this.setState({ teams });
     });
   }
 
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, teams } = this.state;
     if (!isLoaded) {
       return <div>Loading...</div>;
     }
     return (
-      <div>
-        hi
+      <div className="container">
+        <div className="row">
+          {teams.map((team) => (
+            <div className="col-4 p-2">
+              <div className="card p-4">
+                <img
+                  src={`https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/${team.id}.svg`}
+                  className="card-img-top img-fluid"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{team.name}</h5>
+                  <p className="card-text">fdsaufhsdfiuh</p>
+                  <a href="/" className="btn btn-primary">Go</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+
     );
   }
 }
